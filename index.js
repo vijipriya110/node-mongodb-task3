12f59a680c8e510df240d072d94ddc6e7a9c0f27
vijipriya110 <vijipriya110@gmail.com>
2024-02-10 12:11:36 +0530

    i cors

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { studentsRouter } from "./Routers/students.js";
 import { mentorsRouter } from "./Routers/mentors.js";
-
+import cors from "cors"
 // configure the enviornment
 dotenv.config();
 const PORT = process.env.PORT
 
 const app = express();
 app.use(express.json())//middleware
+app.use(cors());
 
 app.use("/students",studentsRouter)
 app.use("/mentors",mentorsRouter)
